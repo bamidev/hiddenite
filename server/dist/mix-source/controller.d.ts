de933dfdf9ad69dd376e8621cbae8ada42a6d512
Bamidev <bamidev@pm.me>
2026-09-14 14:48:13 +0200

    Ignore server/dist

--- server/dist/mix-source/controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { MixSourceService } from './service';
-export declare class MixSourceController {
-    private readonly service;
-    constructor(service: MixSourceService);
-    putMixSource(): string;
-}
