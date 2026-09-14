e15a5fbb9e3a773a9216bd684b619d33ed1a7efc
Bamidev <bamidev@pm.me>
2026-09-14 16:25:21 +0200

    [AI] Move the Queue interface to its own subpath

--- server/src/mix-source/provider.ts
+++ server/src/mix-source/provider.ts
@@ -1,4 +1,17 @@
+import { Queue } from '../queue/provider';
+
 export interface MixSource {
   id: string;
   name: string;
+}
+
+export class QueuePoolMixSource implements MixSource {
+  id: string;
+  name: string;
+  queues: Queue[] = [];
+
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
 }
