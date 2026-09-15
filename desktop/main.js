6bd22e3085475ab16402992827441e318d7d393e
Bamidev <bamidev@pm.me>
2026-09-15 13:49:38 +0200

    Some starting code for an electron app

---
+++ desktop/main.js
@@ -0,0 +1,16 @@
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadURL('http://localhost:5173')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
