605da7f7076eefd12cdaab759b7d126225102240
Bamidev <bamidev@pm.me>
2026-09-15 14:02:21 +0200

    [AI] Hide the library menu item outside of electron

--- client/src/components/nav-bar.tsx
+++ client/src/components/nav-bar.tsx
@@ -3,24 +3,26 @@ export default function NavBar() {
     <nav className="navbar navbar-expand bg-body-tertiary">
       <div className="container-fluid">
         <span className="navbar-brand mb-0 h1">Party Player</span>
-        <ul className="navbar-nav">
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Library
-            </a>
-            <ul className="dropdown-menu">
-              <li>
-                <a className="dropdown-item" href="#">Rescan</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
+        {window.electron?.isElectron && (
+          <ul className="navbar-nav">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Library
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">Rescan</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        )}
       </div>
     </nav>
   )
