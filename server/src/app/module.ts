3a7f2518e71b19d5ff2f9b10e4836519b2be39ef
Bamidev <bamidev@pm.me>
2026-09-20 20:28:52 +0200

    [AI] Move the dist folder to the root of the repo and let the server serve it

--- server/src/app/module.ts
+++ server/src/app/module.ts
@@ -2,8 +2,6 @@ import { MixSourceModule } from '../mix-source/module';
 import { LibraryModule } from '../library/module';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { AppController } from './controller';
-import { AppService } from './service';
 
 import { join } from 'node:path';
 
@@ -12,10 +10,8 @@ import { join } from 'node:path';
     MixSourceModule,
     LibraryModule,
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
+      rootPath: join(__dirname, '..', '..', '..', 'dist'),
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
