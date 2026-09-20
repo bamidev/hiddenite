092f8201563dff5a7390eac245c4ee9e5cbf2c90
Bamidev <bamidev@pm.me>
2026-09-20 22:29:12 +0200

    [AI] Log all http requests

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,22 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 import { AppModule } from './app/module';
 import { config } from './config';
 import { createWebdavMiddleware } from './webdav/provider';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpLogger = new Logger('HTTP');
+  const server = express();
+  server.use((req, res, next) => {
+    res.on('finish', () => {
+      httpLogger.log(`${req.method} ${req.originalUrl} ${res.statusCode}`);
+    });
+    next();
+  });
+
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.enableCors({
     origin: [`http://localhost:${config.server.port}`, ...config.server.baseUrls],
